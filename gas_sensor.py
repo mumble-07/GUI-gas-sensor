1f8db0d7ed97d3fbab527ee0157ee94049599cf7
Andrea De Guzman <andreapauline.deguzman@gmail.com>
2021-08-11 07:45:08 +0800

    py code

--- gas_sensor.py
+++ gas_sensor.py
@@ -43,13 +43,26 @@ Ro = 24000 #in Ohm
 
 #Calculating volate
 volts = (AnalogIn(ads,ADS.P0) * 3.3) / float(1023)
-print 'volts:', volts
+print; 'volts:', volts
 
 #Calculating Rs of TGS 2600
 Rs = ((Vc*RL)/volts)-RL
-print 'RS: ', Rs
+print; 'RS: ', Rs
 
 #Calculating RS/RO ratio
 Rs_Ro = Rs / Ro
 Rs_Ro = round(Rs_Ro,2)
-print "Rs_Ro: ", Rs_Ro
+print; "Rs_Ro: ", Rs_Ro
+
+#=======CATEGORY==========
+
+if Rs_Ro >= 1:
+  Rs_Ro_stage = "Fresh air"
+elif Rs_Ro < 1 and Rs_Ro >= 0.9:
+  Rs_Ro_stage = ""
+elif Rs_Ro < 0.9 and Rs_Ro >= 0.7:
+  Rs_Ro_stage = ""
+elif Rs_Ro < 0.7 and Rs_Ro >= 0.5:
+  Rs_Ro_stage = ""
+elif Rs_Ro < 0.5:
+  Rs_Ro_stage = ""
